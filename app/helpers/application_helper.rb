e7acef2b4f79a0e39046f4e46d1f0626a2f1106a
karol <k.wylizinski@wp.pl>
2015-11-23 23:21:51 +0100

    add navbar and logos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def logo(image_path, size)
+    if size == "large"
+      actual_size = "1075×335"
+    elsif size == "medium"
+      actual_size = "600×205"
+    else
+      actual_size = "50x50"
+    end
+    image_tag image_path, size: "#{actual_size}"
+  end
 end
