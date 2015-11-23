7f2f1dbd04a63c22e57d3a48288f811dd9833e00
Karol Wyliziński <k.wylizinski@wp.pl>
2015-11-23 15:52:52 +0100

    Facebook login tests

---
+++ spec/features/user_signs_in_via_facebook_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../rails_helper'
+
+feature 'Sign via Facebook' do
+  context 'valid omniauth' do
+    before do
+      set_omniauth
+      visit 'users/sign_up'
+      click_link_or_button 'Sign in with Facebook'
+    end
+
+    scenario 'with successful auth' do
+      expect(page.status_code).to be 200
+      expect(page).to have_content 'foobar@example.com'
+    end
+  end
+
+  context 'invalid omniauth' do
+    before do
+      set_invalid_omniauth
+      visit 'users/sign_up'
+      click_link_or_button 'Sign in with Facebook'
+    end
+
+    scenario 'with failed auth' do
+      expect(page.status_code).to be 200
+      expect(page).to_not have_content 'foobar@example.com'
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,12 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'omniauth'
 require 'capybara/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
+require_relative '../spec/support/omniauth_test_helper'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -31,11 +34,13 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::TestHelpers, type: :controller
+  config.extend OmniAuth, type: :controller
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
