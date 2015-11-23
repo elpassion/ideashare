49984c4f70b684bd8743616f3eafe5d20ac90567
Karol Wyliziński <k.wylizinski@wp.pl>
2015-11-23 11:13:14 +0100

    add devise gem and landing page for signing up/in with tests

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'User signs up' do
+
+  scenario 'with valid email and password' do
+    visit '/'
+    click_link 'Join'
+    fill_in 'Email', with: 'test@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content('You have signed up successfully.')
+  end
+
+  scenario 'with invalid email' do
+    visit '/'
+    click_link 'Join'
+    fill_in 'Email', with: 'test'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content('is invalid')
+  end
+
+  scenario 'with blank password' do
+    visit '/'
+    click_link 'Join'
+    fill_in 'Email', with: 'test@email.com'
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
+  end
+end
