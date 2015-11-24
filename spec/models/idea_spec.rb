9beadbdfdb6f6db599e3679c51267a96c02e96e2
Karol Wyliziński <k.wylizinski@wp.pl>
2015-11-24 14:12:36 +0100

    Model class specs

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Idea, type: :model do
+
+  it 'factory is valid' do
+    expect(build(:idea)).to be_valid
+  end
+
+  describe 'owner' do
+    it 'is assigned to user' do
+      expect(build(:idea, owner: nil)).to_not be_valid
+    end
+  end
+
+  describe 'assignation to desk' do
+    it 'is assigned to desk' do
+      expect(build(:idea, desk: nil)).to_not be_valid
+    end
+  end
+
+  describe 'name' do
+    it 'is required' do
+      expect(build(:idea, name: nil)).to_not be_valid
+    end
+  end
+
+  describe 'if time is up to add new idea' do
+    xit 'will not add idea to an expired desk' do
+      finish_time = 3.hours.from_now(DateTime.current)
+      desk = build(:desk, finish_at: finish_time)
+      Timecop.travel(finish_time)
+      expect(build(:idea, desk: desk)).to_not be_valid
+    end
+  end
+end
